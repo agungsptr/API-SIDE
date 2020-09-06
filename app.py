b191e6a2974d0a68b68ced55701cd65f450b48d0
agungsptr <agung.eka1999@gmail.com>
2020-09-06 14:34:42 +0000

    change comment

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ url_prefix = '/api/v1'
 app = Flask(__name__)
 app.register_blueprint(res.user_api, url_prefix=url_prefix)
 
-# ACCESS_TOKEN_JWT
+# JWT CONFIG
 app.config['SECRET_KEY'] = "7Bs6vcBuoJQ97XnKNUbO2C4wvdwGPIzj14JQE3k2fdDdt0ihTJcbA" \
                            "PuDtyxmzbL_SIfaStBM3lUyFN0SJxdGwJ4hoZd-UjVsDWmmtU4Yot" \
                            "uSg_hMUz5DRJKtDiRLo0LQghjwQpqveot9w8G5as7JR7ppvVdZfcM" \
@@ -20,7 +20,7 @@ app.config['JWT_BLACKLIST_ENABLED'] = True
 app.config['JWT_BLACKLIST_TOKEN_CHECK'] = ['access', 'refresh']
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(days=1)
 
-# LOGOUT
+# Set blacklist token for loging out
 jwt = JWTManager(app)
 blacklist = set()
 
