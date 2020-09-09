75fb0dbf366eb3a73e2bb10d2ba77d9b108f7d34
agungsptr <agung.eka1999@gmail.com>
2020-09-09 20:14:12 +0000

    setting up for heroku

--- models/model.py
+++ models/model.py
@@ -5,7 +5,8 @@ db = PostgresqlDatabase(
     database=pd.DB_NAME,
     user=pd.DB_USERNAME,
     host=pd.DB_HOST,
-    port=pd.DB_PORT)
+    port=pd.DB_PORT,
+    password=pd.DB_PASSWORD)
 
 
 class BaseModel(Model):
