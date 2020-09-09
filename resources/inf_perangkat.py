d6c211c788bd925c2959d505c3dcf957930e4f2c
agungsptr <agung.eka1999@gmail.com>
2020-09-09 00:13:27 +0000

    add resource inf_perangkat and inf_umum

---
+++ resources/inf_perangkat.py
@@ -0,0 +1,122 @@
+from flask import Blueprint
+from flask_jwt_extended import jwt_required
+from flask_restful import Resource, Api, reqparse, fields, marshal
+
+from .resource import *
+
+ip_fields = {
+    'id': fields.Integer,
+    'kades': fields.String,
+    'sekdes': fields.Integer,
+    'ku_tata_usaha': fields.String,
+    'ku_keuangan': fields.String,
+    'ku_perencanaan': fields.String,
+    'ku_pemerintahan': fields.String,
+    'ku_kesejahteraan': fields.String,
+    'ku_pelayanan': fields.String
+}
+
+
+def get_or_abort(id):
+    try:
+        query = models.InfPerangkat.get_by_id(id)
+    except models.InfPerangkat.DoesNotExist:
+        abort(404)
+    else:
+        return query
+
+
+class BaseIp(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        super().__init__()
+
+    def reqargs(self):
+        self.reqparse.add_argument(
+            'kades',
+            required=False, location=['form', 'json'])
+        self.reqparse.add_argument(
+            'sekdes',
+            required=False, location=['form', 'json'])
+        self.reqparse.add_argument(
+            'ku_tata_usaha',
+            required=False, location=['form', 'json'])
+        self.reqparse.add_argument(
+            'ku_keuangan',
+            required=False, location=['form', 'json'])
+        self.reqparse.add_argument(
+            'ku_perencanaan',
+            required=False, location=['form', 'json'])
+        self.reqparse.add_argument(
+            'ku_pemerintahan',
+            required=False, location=['form', 'json'])
+        self.reqparse.add_argument(
+            'ku_kesejahteraan',
+            required=False, location=['form', 'json'])
+        self.reqparse.add_argument(
+            'ku_pelayanan',
+            required=False, location=['form', 'json'])
+
+
+class GetPost(BaseIp):
+    # index
+    @jwt_required
+    def get(self):
+        ip = [marshal(ip, ip_fields)
+              for ip in models.InfPerangkat.select()]
+        return {'success': True,
+                'data': ip}
+
+    # store
+    @jwt_required
+    def post(self):
+        self.reqargs()
+
+        args = self.reqparse.parse_args()
+
+        try:
+            ip = models.InfPerangkat.create(**args)
+            return {'success': True,
+                    'data': marshal(ip, ip_fields)}
+        except models.InfPerangkat.DoesNotExist:
+            return {'success': False,
+                    'message': 'Model does not exist'}
+
+
+class GetPutDel(BaseIp):
+    # show
+    @jwt_required
+    def get(self, id):
+        ip = get_or_abort(id)
+        return {'success': True,
+                'data': marshal(ip, ip_fields)}
+
+    # edit
+    @jwt_required
+    def put(self, id):
+        self.reqargs()
+
+        get_or_abort(id)
+        args = self.reqparse.parse_args()
+
+        try:
+            models.InfPerangkat.update(**args).where(models.InfPerangkat.id == id).execute()
+            return {'success': True,
+                    'data': marshal(get_or_abort(id), ip_fields)}
+        except models.InfPerangkat.DoesNotExist:
+            return {'success': False,
+                    'message': 'Model does not exist'}
+
+    # delete
+    @jwt_required
+    def delete(self, id):
+        get_or_abort(id)
+        models.InfPerangkat.delete().where(models.InfPerangkat.id == id).execute()
+        return {'success': True,
+                'message': "Info Perangkat Desa is deleted"}
+
+
+inf_perangkat_api = Blueprint('resources.inf_perangkat', __name__)
+api = Api(inf_perangkat_api)
+api.add_resource(GetPost, '/inf-perangkat', endpoint='inf-perangkat/gp')
+api.add_resource(GetPutDel, '/inf-perangkat/<int:id>', endpoint='inf-perangkat/gpd')
---
+++ resources/inf_umum.py
@@ -0,0 +1,126 @@
+from flask import Blueprint
+from flask_jwt_extended import jwt_required
+from flask_restful import Resource, Api, reqparse, fields, marshal
+
+from .resource import *
+
+umum_fields = {
+    'id': fields.Integer,
+    'luas_desa': fields.Float,
+    'total_dusun': fields.Integer,
+    'bw_utara': fields.String,
+    'bw_selatan': fields.String,
+    'bw_timur': fields.String,
+    'bw_barat': fields.String,
+    'jp_kecamatan': fields.Float,
+    'jp_kabupaten': fields.Float,
+    'jp_provinsi': fields.Float
+}
+
+
+def get_or_abort(id):
+    try:
+        query = models.InfUmum.get_by_id(id)
+    except models.InfUmum.DoesNotExist:
+        abort(404)
+    else:
+        return query
+
+
+class BaseUmum(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        super().__init__()
+
+    def reqargs(self):
+        self.reqparse.add_argument(
+            'luas_desa', type=float,
+            required=False, location=['form', 'json'])
+        self.reqparse.add_argument(
+            'total_dusun', type=int,
+            required=False, location=['form', 'json'])
+        self.reqparse.add_argument(
+            'bw_utara',
+            required=False, location=['form', 'json'])
+        self.reqparse.add_argument(
+            'bw_selatan',
+            required=False, location=['form', 'json'])
+        self.reqparse.add_argument(
+            'bw_timur',
+            required=False, location=['form', 'json'])
+        self.reqparse.add_argument(
+            'bw_barat',
+            required=False, location=['form', 'json'])
+        self.reqparse.add_argument(
+            'jp_kecamatan', type=float,
+            required=False, location=['form', 'json'])
+        self.reqparse.add_argument(
+            'jp_kabupaten', type=float,
+            required=False, location=['form', 'json'])
+        self.reqparse.add_argument(
+            'jp_provinsi', type=float,
+            required=False, location=['form', 'json'])
+
+
+class GetPost(BaseUmum):
+    # index
+    @jwt_required
+    def get(self):
+        umum = [marshal(umum, umum_fields)
+                for umum in models.InfUmum.select()]
+        return {'success': True,
+                'data': umum}
+
+    # store
+    @jwt_required
+    def post(self):
+        self.reqargs()
+
+        args = self.reqparse.parse_args()
+
+        try:
+            umum = models.InfUmum.create(**args)
+            return {'success': True,
+                    'data': marshal(umum, umum_fields)}
+        except models.InfUmum.DoesNotExist:
+            return {'success': False,
+                    'message': 'Model does not exist'}
+
+
+class GetPutDel(BaseUmum):
+    # show
+    @jwt_required
+    def get(self, id):
+        umum = get_or_abort(id)
+        return {'success': True,
+                'data': marshal(umum, umum_fields)}
+
+    # edit
+    @jwt_required
+    def put(self, id):
+        self.reqargs()
+
+        get_or_abort(id)
+        args = self.reqparse.parse_args()
+
+        try:
+            models.InfUmum.update(**args).where(models.InfUmum.id == id).execute()
+            return {'success': True,
+                    'data': marshal(get_or_abort(id), umum_fields)}
+        except models.InfUmum.DoesNotExist:
+            return {'success': False,
+                    'message': 'Model does not exist'}
+
+    # delete
+    @jwt_required
+    def delete(self, id):
+        get_or_abort(id)
+        models.InfUmum.delete().where(models.InfUmum.id == id).execute()
+        return {'success': True,
+                'message': "Info Umum Desa is deleted"}
+
+
+inf_umum_api = Blueprint('resources.inf_umum', __name__)
+api = Api(inf_umum_api)
+api.add_resource(GetPost, '/inf-umum', endpoint='inf-umum/gp')
+api.add_resource(GetPutDel, '/inf-umum/<int:id>', endpoint='inf-umum/gpd')
