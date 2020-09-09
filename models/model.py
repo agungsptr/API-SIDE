2cd11eef5b8a9daea3e5afd281bdd548e09ba704
agungsptr <agung.eka1999@gmail.com>
2020-09-09 21:10:48 +0000

    done setting configuration model using postgresql for production and mysql for development

--- models/model.py
+++ models/model.py
@@ -1,12 +1,25 @@
+from flask import Flask
 from peewee import *
-from config import Production as pd
+from config import *
 
-db = PostgresqlDatabase(
-    database=pd.DB_NAME,
-    user=pd.DB_USERNAME,
-    host=pd.DB_HOST,
-    port=pd.DB_PORT,
-    password=pd.DB_PASSWORD)
+config = {}
+
+app = Flask(__name__)
+
+if app.config["ENV"] == "production":
+    db = PostgresqlDatabase(
+        database=Production.DB_NAME,
+        user=Production.DB_USERNAME,
+        host=Production.DB_HOST,
+        port=Production.DB_PORT,
+        password=Production.DB_PASSWORD)
+else:
+    db = MySQLDatabase(
+        database=Development.DB_NAME,
+        user=Development.DB_USERNAME,
+        host=Development.DB_HOST,
+        port=Development.DB_PORT,
+        password=Development.DB_PASSWORD)
 
 
 class BaseModel(Model):
